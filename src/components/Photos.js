ae703c2797ef125cccc2f5771121e0e2576419e2
drzackyll <adams.z.d@gmail.com>
2017-02-13 16:56:40 -0500

    Tries different means of using photo slider; not working yet

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,56 +1,69 @@
-import React from "react"
+import React, { Component } from "react"
 import { slick } from "slick-carousel"
+import { Slider } from "react-slick"
 import $ from "jquery"
 
-const Photos = () => {
-  $(document).ready(function(){
-    $('.responsive').slick({
+class Photos extends Component {
+
+  // $(document).ready(function(){
+  //   $('.responsive').slick({
+  //     dots: true,
+  //     infinite: false,
+  //     speed: 300,
+  //     slidesToShow: 4,
+  //     slidesToScroll: 4,
+  //     responsive: [
+  //       {
+  //         breakpoint: 1024,
+  //         settings: {
+  //           slidesToShow: 3,
+  //           slidesToScroll: 3,
+  //           infinite: true,
+  //           dots: true
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 600,
+  //         settings: {
+  //           slidesToShow: 2,
+  //           slidesToScroll: 2
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 480,
+  //         settings: {
+  //           slidesToShow: 1,
+  //           slidesToScroll: 1
+  //         }
+  //       }
+  //     ]
+  //   })
+  // })
+  function () {
+    var settings = {
       dots: true,
-      infinite: false,
+      infinite: true,
       speed: 300,
       slidesToShow: 4,
-      slidesToScroll: 4,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    })
-  })
+      slidesToScroll: 4
+    };
+  }
+  render () {
 
-  return (
-    <div className="responsive">
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2738.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2747.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2758.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2773.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2786.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2821.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2830.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2839.jpg")} className="picture-resize" alt="work!" /></div>
-      <div className="responsive"><img src={require("../../public/images/photos/IMG_2866.jpg")} className="picture-resize" alt="work!" /></div>
-    </div>
-  )
+      return (
+        <Slider {...settings}>
+        <div><img src={require("../../public/images/photos/IMG_2738.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2747.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2758.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2773.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2786.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2821.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2830.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2839.jpg")} className="picture-resize" alt="work!" /></div>
+        <div><img src={require("../../public/images/photos/IMG_2866.jpg")} className="picture-resize" alt="work!" /></div>
+        </Slider>
+      )
+  }
 }
 
 export default Photos
