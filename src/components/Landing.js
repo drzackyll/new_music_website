a0546d840b84c5f05c1166d95283ad644675444b
drzackyll <adams.z.d@gmail.com>
2017-03-29 18:36:45 -0400

    Fixes some CSS

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,14 +7,17 @@ const spotifyStyle = {
 
 const Landing = () => {
   return (
-    <div>
-      Hello.<br />
-      <img src={require("../../public/images/photos/forethought.jpg")} className="picture-resize" alt="Forethougt EP" /><br />
-      Checkout Zack Adams debut EP, <i>Forethought</i> now on the following platforms: <br />
-      <a href="https://itunes.apple.com/us/album/forethought-ep/id1103053880?app=itunes"><img src={require("../../public/images/Get_it_on_iTunes_Badge_US_1114.svg")} alt="Get it on iTunes"/></a>
-      <a href="https://itunes.apple.com/us/album/forethought-ep/id1103053880?app=music"><img src={require("../../public/images/US_Listen_on_Apple_Music_Badge.svg")} alt="Listen on Apple Music"/></a>
-      <iframe src="https://embed.spotify.com/follow/1/?uri=spotify:artist:5jyjga1JavnVapUhte4Nj5&size=basic&theme=light&show-count=0" width="200" height="25" scrolling="no" frameBorder="0" style={spotifyStyle} allowTransparency="true"></iframe>
-      <script src="https://cashmusic.org/public/cashmusic.js" data-element="11688"></script>
+    <div className="container">
+      <div className="row">
+        <div className="ten columns offset-by-one">
+          <img src={require("../../public/images/photos/forethought.jpg")} className="picture-resize" alt="Forethougt EP" /><br />
+          Checkout Zack Adams debut EP, <i>Forethought</i> now on the following platforms: <br />
+          <a href="https://itunes.apple.com/us/album/forethought-ep/id1103053880?app=itunes"><img src={require("../../public/images/Get_it_on_iTunes_Badge_US_1114.svg")} alt="Get it on iTunes"/></a>
+          <a href="https://itunes.apple.com/us/album/forethought-ep/id1103053880?app=music"><img src={require("../../public/images/US_Listen_on_Apple_Music_Badge.svg")} alt="Listen on Apple Music"/></a>
+          <iframe src="https://embed.spotify.com/follow/1/?uri=spotify:artist:5jyjga1JavnVapUhte4Nj5&size=basic&theme=light&show-count=0" width="200" height="25" scrolling="no" frameBorder="0" style={spotifyStyle} allowTransparency="true"></iframe>
+          <script src="https://cashmusic.org/public/cashmusic.js" data-element="11688"></script>
+        </div>
+      </div>
     </div>
   )
 }
