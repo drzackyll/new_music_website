98e998cde30dbcc532c93ead17fd8650f452f9e6
drzackyll <adams.z.d@gmail.com>
2017-04-03 23:25:30 -0400

    Adds photo to About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="lyrics">
-    Zack Adams is a songwriter and singer living in Brooklyn, New York. He was
-    born in Clinton, New York, and raised in North Carolina and Kentucky. In his
-    early years, Adams was encouraged to explore music: first as a piano student,
-    then as a choir boy, later as a saxophone player. He fell for rock and roll
-    during his adolescence, then taught himself how to play his college roommate's
-    guitar. His debut EP, <i>Forethought</i> was released in 2016.
-
+    <div>
+      <img style={{height: "40%", width: "40%"}} src={require("../../public/images/photos/thumb_IMG_2622_1024.jpg")} alt="Zack Adams"/>
+      <div className="lyrics">
+        Zack Adams is a songwriter and singer living in Brooklyn, New York. He was
+        born in Clinton, New York, and raised in North Carolina and Kentucky. In his
+        early years, Adams was encouraged to explore music: first as a piano student,
+        then as a choir boy, later as a saxophone player. He fell for rock and roll
+        during his adolescence, then taught himself how to play his college roommate's
+        guitar. His debut EP, <i>Forethought</i> was released in 2016.
+      </div>
     </div>
   )
 }
