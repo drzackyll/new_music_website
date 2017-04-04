94094553adfc8565685d8eac2010b26ffc3f631b
drzackyll <adams.z.d@gmail.com>
2017-04-03 23:17:56 -0400

    Deletes some photos, updates About

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -57,10 +57,7 @@ const Photos = () => {
       <div><img src={require("../../public/images/photos/IMG_2747.jpg")} className="picture-resize" alt="work!" /></div>
       <div><img src={require("../../public/images/photos/IMG_2758.jpg")} className="picture-resize" alt="work!" /></div>
       <div><img src={require("../../public/images/photos/IMG_2773.jpg")} className="picture-resize" alt="work!" /></div>
-      <div><img src={require("../../public/images/photos/IMG_2786.jpg")} className="picture-resize" alt="work!" /></div>
       <div><img src={require("../../public/images/photos/IMG_2821.jpg")} className="picture-resize" alt="work!" /></div>
-      <div><img src={require("../../public/images/photos/IMG_2830.jpg")} className="picture-resize" alt="work!" /></div>
-      <div><img src={require("../../public/images/photos/IMG_2839.jpg")} className="picture-resize" alt="work!" /></div>
       <div><img src={require("../../public/images/photos/IMG_2866.jpg")} className="picture-resize" alt="work!" /></div>
       </div>
     )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="one column offset-by-seven" style={{textAlign: "center"}}><Link to="/landing">home</Link></div>
+        <div className="one column offset-by-seven" style={{textAlign: "center"}}><Link to="/">home</Link></div>
         <div className="one column" style={{textAlign: "center"}}><Link to="/music">music</Link></div>
         <div className="one column" style={{textAlign: "center"}}><Link to="/photos">pics</Link></div>
         <div className="one column" style={{textAlign: "center"}}><Link to="/about">about</Link></div>
